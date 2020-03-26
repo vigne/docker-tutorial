ff3b07c03702d813c8a777066619d1b4a0862f07
Ralph Vigne <ralph@donald.home>
2020-03-26 20:17:10 +0100

    removed SLR for now

--- docker-tutorial:latest/SomethingLikeReddit/backend/.flaskenv
+++
@@ -1,4 +0,0 @@
-FLASK_APP=app.py
-
-# auto restarts the app on file change
-FLASK_DEBUG=1
--- docker-tutorial:latest/SomethingLikeReddit/backend/app.py
+++
@@ -1,5 +0,0 @@
-from server import api, routes
-
-if __name__ == '__main__':
-    api.debug = False
-    api.run(host = api.config.get('FLASK_HOST'), port=5005)
